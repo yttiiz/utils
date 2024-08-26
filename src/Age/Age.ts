cc565465bbac3e6ad0dabbbc8320b1f205c9831c
yttiiz <dominique.talis@proton.me>
2024-08-26 21:40:26 +0200

    feat(age): create class 'Age' ans its methods

---
+++ src/Age/Age.ts
@@ -0,0 +1,31 @@
+export class Age {
+	/**
+	 * Returns age according to the given `date` parameter.
+	 * @param date
+	 */
+	public static get(date: string) {
+		return new Date(Date.now() - new Date(date).getTime()).getFullYear() - 1970;
+	}
+	/**
+	 * Returns age with years according to the given `date` parameter.
+	 * @param date
+	 */
+	public static getWithYear(date: string, locale: "FR" | "EN" = "FR") {
+		const age = Age.get(date);
+		const s = age >= 2 ? "s" : "";
+		const years = locale === "FR" ? `an${s}` : `year${s} old`;
+		return `${age} ${years}`;
+	}
+
+	/**
+	 * Returns the oldest between two dates.
+	 * @param firstDate
+	 * @param secondDate
+	 */
+	public static whichIsOlder(firstDate: string, secondDate: string) {
+		const age1 = new Date(firstDate).getTime();
+		const age2 = new Date(secondDate).getTime();
+
+		return age1 > age2 ? firstDate : age1 === age2 ? "same dates" : secondDate;
+	}
+}
